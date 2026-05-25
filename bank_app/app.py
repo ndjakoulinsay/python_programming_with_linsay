d96623ef5cf0e564933cddaa606783c621af0355
ndjakoulinsay <ndjakoulinsay@gmail.com>
2026-05-25 16:24:08 +0100

    Update app.py

--- bank_app/app.py
+++ bank_app/app.py
@@ -66,14 +66,23 @@ def deposit():
     if cus_account is None:
         print(f"Account {account_number} not found.")
         return
+    default_account = find_account(DEFAULT_BANK_ACCOUNT_NUMBER)
+    if amount > default_account.balance:
+        print(
+            f"Insufficient funds in the default bank account for deposit. Available balance: {default_account.balance}"
+        )
+        return
     if cus_account.balance + cus_account.account_limit >= amount:
+        print(
+            f"Deposit amount exceeds account limit for account {account_number}. Available balance and account limit: {cus_account.balance + cus_account.account_limit}"
+        )
         cus_account.balance += (
             amount# Add the deposited amount to the customer's account balance
         )
-    default_account = find_account(DEFAULT_BANK_ACCOUNT_NUMBER)
-    default_account.balance -= (
+        default_account = find_account(DEFAULT_BANK_ACCOUNT_NUMBER)
+        default_account.balance -= (
         amount  # Subtract the deposited amount from the default bank account
-    )
+        )
     record_transaction(
         DEFAULT_BANK_ACCOUNT_NUMBER, account_number, amount
     )  # Record the deposit transaction
@@ -91,6 +100,9 @@ def withdraw():
         print(f"Account {account_number} not found.")
         return
     if cus_account.balance + cus_account.overdraft_limit >= amount:
+        print(
+            f"Withdrawal amount exceeds available balance and overdraft limit for account {account_number}. Available balance and overdraft limit: {cus_account.balance + cus_account.overdraft_limit}"
+        )
         cus_account.balance -= (amount
         )  # Subtract the withdrawn amount from the customer's account balance
         default_account.balance += (
